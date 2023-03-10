210431807773de85536a133de64e5d51c39441c3
Jaakko Pyrhönen <jaakkopyrhonen@gmail.com>
2023-03-10 11:16:28 +0200

    first commit

---
+++ app/src/main/java/com/example/movietrackerv2/data/xml_tool/Parser.java
@@ -0,0 +1,16 @@
+package com.example.movietrackerv2.data.xml_tool;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class Parser implements ParserInterface {
+    // parses the given document to a NodeList by the given tag. Returns the list.
+    public NodeList parseToNodeList(Document doc, String tag) {
+        try {
+            doc.getDocumentElement().normalize();
+            return doc.getDocumentElement().getElementsByTagName(tag);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+}
